811e3896a86ad7f29f9a4bc640c76182915dc0f9
omerkolcak <o.faruk.kolcak@gmail.com>
2021-12-27 17:05:32 +0300

    random data

--- src/synthetic_data_generator/generate_text.py
+++ src/synthetic_data_generator/generate_text.py
@@ -13,29 +13,6 @@ def generate_random_name(amount):
 
     return list_random_name
 
-def generate_random_date(amount, min_year=1930, max_year=2022):
-
-    list_random_date = []
-
-    for i in range(amount):
-        year = random.randint(min_year, max_year)
-        month = random.randint(1,12)
-
-        if month == 12 or month == 10 or month == 8 or month == 7 or month == 5 or month == 3 or month == 1:
-            day = random.randint(1,31)
-        else:
-            if month == 2:
-                if year % 4 == 0:
-                    day = random.randint(1,29)
-                else:
-                    day = random.randint(1,28)
-            else:
-                day = random.randint(1,30)
-
-        date = str(day) + "/" + str(month) + "/" + str(year)
-        list_random_date.append(date)
-
-    return list_random_date
 
 def generate_random_company_name(amount):
 
@@ -53,11 +30,10 @@ def generate_random_address(amount):
 
     for i in range(amount):
 
-        zip_code, city, state = gen_data.create_city_state_zip()
+        zip_code, _, state = gen_data.create_city_state_zip()
         street = gen_data.create_street()
 
-        address = city + " " + street + " " + state + " " + zip_code
-        print(address)
+        address =  " " + street + " " + state + " " + zip_code
         list_random_address.append(address)
 
     # fake = Faker()
@@ -68,6 +44,157 @@ def generate_random_address(amount):
     #     list_random_address.append(address)
 
     return list_random_address
+
+def generate_random_email(amount, keywords = None):
+
+    list_random_email = []
+
+    if keywords:
+        
+        for i in range(amount):
+            k_word = tuple(keywords[i].split())
+            email = gen_data.create_email(name = k_word)
+            list_random_email.append(email)
+    else:
+        for i in range(amount):
+            email = gen_data.create_email()
+            list_random_email.append(email)
+
+
+    return list_random_email
+
+def generate_random_website(amount, keywords = None):
+
+    list_random_website = []
+    if keywords:
+        for i in range(amount):
+            k_word = tuple(keywords[i].split())
+            user_choices = ["%s.%s" % (k_word[0], k_word[1]), "%s" % k_word[0], "%s.%s" % (k_word[0][:1], k_word[1]), "%s%s" %(random.choice(gen_data.latin_words), random.randint(0,9999))]
+            domain = random.choice(gen_data.email_domains)
+            website = ("%s.%s" % (random.choice(user_choices), domain)).lower()
+            list_random_website.append(website)
+
+    else:
+        for i in range(amount):
+            choices = ["%s.%s" % (random.choice(gen_data.latin_words), random.randint(0,9999)), "%s" %(random.choice(gen_data.latin_words))]
+            domain = random.choice(gen_data.email_domains)
+            website = ("%s.%s" % (random.choice(choices), domain)).lower()
+            list_random_website.append(website)
+
+    return list_random_website
+
+def generate_random_industry(amount, sector = None):
+
+    dict_industry_sector = {
+        'Technology' : ['Software', 'Hardware', 'Electronics', 'Graphics', 'Application', 'Virtual', 'Electronic', 'Data'],
+        'Telecommunications' : ['Telecom', 'Internet', 'Network'],
+        'Consulting' : ['Research', 'Analysis', 'Contract', 'Solutions'],
+        'Construction' : ['Architecture', 'Building'],
+        'Health' : ['Medicine'],
+        'Entertainment' : ['Adventure']
+    }
+
+    list_random_industries = []
+
+    industries = dict_industry_sector.keys()     
+
+    if sector:
+        for i in range(amount):
+            industry_found = False
+            for industry in industries:
+                if sector[i] in dict_industry_sector[industry] and not industry_found:
+                    list_random_industries.append(industry)
+                    industry_found = True
+            
+            if not industry_found:
+                list_random_industries.append(random.choice(industries))
+
+    else:      
+        for i in range(amount):
+            industry = random.choice(industries)
+            list_random_industries.append(industry)
+
+    return list_random_industries
+
+def generate_random_sector(amount, keywords = None):
+    sectors = ['Telecom', 'Software', 'Hardware', 'Electronics', 'Research', 'Architecture', 'Building', 'Medicine',
+    'Graphics', 'Analysis', 'Contract', 'Solutions', 'Internet', 'Application', 'Virtual', 'Network', 'Electronic', 'Data', 'Adventure']
+
+    list_random_sector = []
+    if keywords:
+        for i in range(amount):
+            sector_found = False
+            for word in keywords[i].split():
+                if word in sectors and not sector_found:
+                    list_random_sector.append(word)
+                    sector_found = True
+            
+            if not sector_found:
+                sector = random.choice(sectors)
+                list_random_sector.append(sector)
+    else:
+        for i in range(amount):
+            sector = random.choice(sectors)
+            list_random_sector.append(sector)
+
+    return list_random_sector
+
+def generate_random_job_title(amount):
+
+    list_random_job_title = []
+    for i in range(amount):
+        job_title = gen_data.create_job_title()
+        list_random_job_title.append(job_title)
+
+    return list_random_job_title
+
+def generate_random_region(amount):
+
+    list_random_region = []
+
+    regions = ['US','EU','AS']
+    for i in range(amount):
+        list_random_region.append(random.choice(regions))
+    
+    return list_random_region
+
+def generate_random_country(amount, region_list = None):
+
+    countries_by_region = {
+        'EU' : ["Andorra", "Albania", "Armenia", "Austria", "Bosnia and Herzegovina", "Belgium", "Bulgaria", "Belarus", "Switzerland", "Cyprus", "Czech Republic", "Germany", "Denmark", "Estonia", "Spain", "Finland", "Faroe Islands", "France", "France, Metropolitan" , "United Kingdom", "Georgia"],
+        'US' : ["Antigua and Barbuda", "Anguilla", "Barbados", "Bermuda", "Brazil", "Canada", "Colombia", "United States"],
+        'AS' : ["Afghanistan", "Antarctica", "Australia", "Azerbaijan", "Bangladesh", "Vietnam", "Vanuatu"] 
+    }
+
+    list_random_country = []
+    if region_list:
+        for i in range(amount):
+            country = random.choice(countries_by_region[region_list[i]])
+            list_random_country.append(country)
+    else:
+        regions = ['EU', 'US', 'AS']
+        
+        for i in range(amount):
+            region = random.choice(regions)
+
+            country = random.choice(countries_by_region[region])
+            list_random_country.append(country)
+
+    return list_random_country
+
+def generate_random_phone_number(amount):
+
+    list_random_phone_number = []
+    faker = Faker()
+
+    for i in range(amount):
+        phone_number = faker.phone_number()
+        list_random_phone_number.append(phone_number)
+    
+    return list_random_phone_number
+
+
+        
 
 
 
--- tests/test_generate_text_utils.py
+++ tests/test_generate_text_utils.py
@@ -4,3 +4,9 @@ def not_contain_numeric(name_list):
             if ch.isnumeric():
                 return False
     return True
+
+def email_contains_at(email_list):
+    for email in email_list:
+        if "@" not in email:
+            return False
+    return True
